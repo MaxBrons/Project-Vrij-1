91edcf467bf65f2bb94cd62ee84013edcf8a7dca
MaxBrons <43110145+MaxBrons@users.noreply.github.com>
2023-05-31 16:42:59 +0200

    Namespaces toegevoegd

--- Assets/_PV1/Assets/Player/Scripts/CameraController.cs
+++ Assets/_PV1/Assets/Player/Scripts/CameraController.cs
@@ -2,24 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraController : MonoBehaviour
+namespace PV
 {
-    [SerializeField] private GameObject m_ObjectToFollow;
-    [SerializeField] private float m_Speed;
-    [SerializeField] private float m_FollowTime;
-    [SerializeField] private Vector3 m_Offset;
-
-    private Vector3 m_Velocity;
-    private Rigidbody m_RB;
-
-    private void Start()
+    public class CameraController : MonoBehaviour
     {
-        m_RB = GetComponent<Rigidbody>();
-    }
+        [SerializeField] private GameObject m_ObjectToFollow;
+        [SerializeField] private float m_Speed;
+        [SerializeField] private float m_FollowTime;
+        [SerializeField] private Vector3 m_Offset;
 
-    private void Update()
-    {
-        Vector3 damp = Vector3.SmoothDamp(transform.position, m_ObjectToFollow.transform.position + m_Offset, ref m_Velocity, m_FollowTime, m_Speed);
-        m_RB.MovePosition(new Vector3(damp.x, damp.y, damp.z));
+        private Vector3 m_Velocity;
+        private Rigidbody m_RB;
+
+        private void Start()
+        {
+            m_RB = GetComponent<Rigidbody>();
+        }
+
+        private void Update()
+        {
+            Vector3 damp = Vector3.SmoothDamp(transform.position, m_ObjectToFollow.transform.position + m_Offset, ref m_Velocity, m_FollowTime, m_Speed);
+            m_RB.MovePosition(new Vector3(damp.x, damp.y, damp.z));
+        }
     }
 }
