c7936945a716e693a719e0ff69053d5678854139
MaxBrons <43110145+MaxBrons@users.noreply.github.com>
2023-06-01 12:55:28 +0200

    Asset locaties veranderd/ Cinemachine & Spline package toegevoegd/  Main Scene update

--- Assets/_PV1/Assets/Player/Scripts/PlayerMovement.cs
+++ Assets/_PV1/Assets/Player/Scripts/PlayerMovement.cs
@@ -1,9 +1,8 @@
-using System.Collections;
+using PV.Input;
+using PV.Utility;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using PV.Utility;
-using System;
 
 namespace PV
 {
